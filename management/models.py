755644fded7d389cda19b8c8457c121401a96656
anxu <axu.home@gmail.com>
2017-06-17 16:09:19 +0800

    设置Query表error_messages可以为空，给Query表添加unicode方法

--- management/models.py
+++ management/models.py
@@ -13,5 +13,8 @@ class User(AbstractUser):
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
     modified = models.DateTimeField(verbose_name="修改时间", auto_now=True)
 
+    def __unicode__(self):
+        return self.username
+
     class Meta:
         ordering = ('-modified',)
--- performance/models.py
+++ performance/models.py
@@ -11,8 +11,11 @@ class Query(models.Model):
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
     duration = models.FloatField(verbose_name="执行时长", default=0.0)
     total = models.IntegerField(verbose_name="查询结果数量", default=0)
-    error_messages = models.TextField(verbose_name="查询错误", default="")
+    error_messages = models.TextField(verbose_name="查询错误", blank=True, default="")
     user = models.ForeignKey(User, verbose_name="执行人")
+
+    def __unicode__(self):
+        return self.query
 
     class Meta:
         ordering = ('-created',)
