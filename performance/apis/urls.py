e00affeba52f985f731d6dfbdca35befc65e2666
anxu <axu.home@gmail.com>
2017-06-20 10:31:29 +0800

    Query调整接口，只开放创建、列表和详情功能

--- performance/apis/urls.py
+++ performance/apis/urls.py
@@ -5,9 +5,15 @@ from rest_framework.routers import DefaultRouter
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'querys', QueryViewSet)
+router.register(r"querys", QueryViewSet)
 
 
 urlpatterns = [
     url(r"^", include(router.urls)),
+    # POST /api/performance/querys/
+    # url(r"^querys/$", QueryViewSet.as_view({"post": "create"}), name="query_create"),
+    # # GET /api/performance/querys/
+    # url(r"^querys/$", QueryViewSet.as_view({"get": "list", "post": "create"}), name="query"),
+    # # GET /api/performance/querys/1/
+    # url(r"^querys/(?P<pk>\d+)/$", QueryViewSet.as_view({"get": "retrieve"}), name="query_detail"),
 ]
--- performance/apis/views.py
+++ performance/apis/views.py
@@ -1,10 +1,14 @@
 # -*- coding: UTF-8 -*-
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from serializers import QuerySerializer
 from performance.models import Query
 
 
-class QueryViewSet(viewsets.ModelViewSet):
+class QueryViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.RetrieveModelMixin,
+                   viewsets.GenericViewSet):
+
     serializer_class = QuerySerializer
     queryset = Query.objects.all()
 
@@ -15,3 +19,7 @@ class QueryViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         excuted_query = serializer.validated_data.get("query")
         serializer.save(excuted_query=excuted_query, user=self.request.user)
+
+    def list(self, request):
+        reponses = super(QueryViewSet, self).list(request)
+        return reponses
