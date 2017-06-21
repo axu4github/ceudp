943becc2cdc7bc86c72cba4f21ef03631dd4755d
anxu <axu.home@gmail.com>
2017-06-21 14:31:12 +0800

    Query表总数列和执行查询列改名

--- performance/models.py
+++ performance/models.py
@@ -3,23 +3,26 @@ from __future__ import unicode_literals
 from django.db import models
 from management.models import User
 from performance.settings import settings
+from performance.databases.backends.sparksql import SparkSQL
 
 """
-字段类型参考：
-https://docs.djangoproject.com/en/1.11/ref/models/fields/
+# 参考文档
+- [字段类型](https://docs.djangoproject.com/en/1.11/ref/models/fields/)
 """
-
 
 class Query(models.Model):
     """查询表"""
     query = models.TextField(verbose_name="查询内容", blank=False)
-    excuted_query = models.TextField(verbose_name="执行的查询", default="")
+    executed_query = models.TextField(verbose_name="执行的查询", default="")
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
     duration = models.FloatField(verbose_name="执行时长", default=0.0)
-    total = models.IntegerField(verbose_name="查询结果数量", default=0)
+    rows = models.IntegerField(verbose_name="查询结果数量", default=0)
     status = models.CharField(verbose_name="查询状态", max_length=2, choices=settings.QUERY_STATUS_CHOICES, default=settings.FAILED)
     error_messages = models.TextField(verbose_name="查询错误", blank=True, default="")
     user = models.ForeignKey(User, verbose_name="执行人")
+
+    def execute(self):
+        return SparkSQL().sql(self.query)
 
     def __unicode__(self):
         return self.query
