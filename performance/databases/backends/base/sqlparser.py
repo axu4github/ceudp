2add299103b39610545204cdcf069cc62978324b
anxu <axu.home@gmail.com>
2017-06-22 11:41:43 +0800

    添加获取SQL主结构的方法以及测试程序

--- performance/databases/backends/base/sqlparser.py
+++ performance/databases/backends/base/sqlparser.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 import sqlparse
+from sqlparse.tokens import Whitespace, Newline, Keyword, DML, Punctuation
 from ceudp.utilities.loggables import Loggable
-from sqlparse.tokens import Whitespace, Newline
+from collections import OrderedDict
 
 """
 # 参考文档
@@ -17,6 +19,7 @@ class BaseSQLParser(Loggable):
         super(BaseSQLParser, self).__init__()
         self.sql_query = sql_query  # 原始SQL
         self.formated = self.format(keyword_case="upper")  # 格式化后的SQL
+        self.main_construction = self.formart_main_construction()
 
     def format(self, **kwargs):
         """
@@ -33,4 +36,64 @@ class BaseSQLParser(Loggable):
             else:
                 sql += item.value
 
-        return sqlparse.format(sql.strip(), **kwargs)
+        sql = sqlparse.format(sql.strip(), **kwargs)
+        # 如果不存在分号（`;`），则在最后添加分号（`;`）。
+        if ";" not in sql and ";" != sql.endswith():
+            sql += ";"
+
+        return sql
+
+    def formart_main_construction(self):
+        """
+        # 获取SQL的主结构（一级）
+
+        # 限制：
+        - 1. 只格式化了一级，没有嵌套格式化
+        - 2. SQL最后必须有分号（`;`）, 因为判断是按照分号（`;`）结算，若没有分号则最后一部分是无法被添加到`DICT`中。
+        - 3. 没有识别`Where`条件
+
+        ```
+        # 举例：
+        原始SQL => "SELECT * FROM table as t WHERE t.a='1' AND t.b='2' GROUP BY t.c ORDER BY t.d DESC LIMIT 1 OFFSET 2;";
+        格式化后为 => 
+        OrderedDict([
+            (u'SELECT', [u' ', u'*', u' ']), 
+            (u'FROM', [u' ', u'TABLE', u' ', u'AS', u' ', u't', u' ', u"WHERE t.a='1' AND t.b='2' "]), 
+            (u'GROUP BY', [u' ', u't.c', u' ']), (u'ORDER BY', [u' ', u't.d DESC', u' ']), 
+            (u'LIMIT', [u' ', u'1', u' ']), 
+            (u'OFFSET', [u' ', u'2']), 
+            (u';', [';'])
+        ])
+        ```
+        """
+        main_construction = OrderedDict()
+        is_append = True # 为了处理`GROUP BY` 和 `ORDER BY` 中间的空格
+        for item in sqlparse.parse(self.formated)[0].tokens:
+            # 处理 `SELECT`
+            if item.ttype is DML and "SELECT" == item.value.upper():
+                tmp_values = []
+                current_keyword = item.value
+            # 处理 `Keyword` (`FORM`, `LIMIT`, `OFFSET`)
+            elif item.ttype is Keyword and item.value.upper() in ["FROM", "LIMIT", "OFFSET"]:
+                main_construction[current_keyword] = tmp_values
+                tmp_values = []
+                current_keyword = item.value
+            # 处理 `Keyword` (`GROUP`, `ORDER`)
+            elif item.ttype is Keyword and item.value.upper() in ["GROUP", "ORDER"]:
+                main_construction[current_keyword] = tmp_values
+                tmp_values = []
+                current_keyword = item.value
+                is_append = False
+            # 处理 `Keyword` (`GROUP BY`, `ORDER BY`)
+            elif item.ttype is Keyword and "BY" == item.value.upper():
+                current_keyword = current_keyword + " " + item.value
+                is_append = True
+            # 处理分号 `;`
+            elif item.ttype is Punctuation and ";" == item.value:
+                main_construction[current_keyword] = tmp_values
+            # 处理值和空格（`Whitespace`）
+            else:
+                if is_append:
+                    tmp_values.append(item.value)
+
+        return main_construction
--- performance/tests.py
+++ performance/tests.py
@@ -7,7 +7,7 @@ import sqlparse
 
 class BaseSQLParserTest(TestCase):
 
-    def test_format(self):
+    def test_format_func(self):
         sql = """
             select username, 
 
@@ -18,6 +18,24 @@ class BaseSQLParserTest(TestCase):
         formated = BaseSQLParser(sql).formated
         self.assertEqual(
             formated, "SELECT username, passwd, * FROM table_a AS ta, A, B, (SELECT * FROM table_b AS tb);", formated)
+
+    def test_formart_main_construction_func(self):
+        sql = """
+            select username, 
+
+
+                passwd, * from table_a as       ta, A,            B, (select      * from  
+                table_b as tb) 
+            group by A.a, B.a    order by tb.a 
+            limit 1
+            offset 2
+                ;
+        """
+        main_construction = BaseSQLParser(sql).main_construction
+        self.assertTrue("ORDER BY" in main_construction, main_construction)
+        self.assertTrue("OFFSET" in main_construction, main_construction)
+        self.assertEqual(
+            2, int("".join(main_construction.get("OFFSET")).strip()), int("".join(main_construction.get("OFFSET")).strip()))
 
 
 class SparkSQLRunEnvTests(TestCase):
