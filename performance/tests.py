94bfa2f45fd52100385e9d58db0cd9c751c32fc0
axu4github <axu.home@gmail.com>
2017-06-24 23:04:58 +0800

    修改测试

--- performance/tests.py
+++ performance/tests.py
@@ -129,5 +129,5 @@ class SparkSQLRunEnvTest(TestCase):
 class SparkSQLClassTest(TestCase):
 
     def test_sql_func(self):
-        print SparkSQL().sql("select count(name) from people group by name order by name desc limit 1;")
-        # pass
+        # print SparkSQL().sql("select * from people  ;")
+        pass
