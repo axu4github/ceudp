5798a2b572cec9f84d5c520bbf20082d6068c984
axu4github <axu.home@gmail.com>
2017-06-24 23:01:24 +0800

    完成查询后数据回写数据库，并完善返回接口

--- performance/apis/views.py
+++ performance/apis/views.py
@@ -1,15 +1,17 @@
 # -*- coding: UTF-8 -*-
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from serializers import QuerySerializer
 from performance.models import Query
 from rest_framework.response import Response
 from ceudp.utilities.loggables import Loggable
 from performance.settings import settings
+from collections import OrderedDict
 
 """
 # 参考文档：
 - [Django REST Framswork](http://www.django-rest-framework.org/)
 """
+
 
 class QueryViewSet(mixins.CreateModelMixin,
                    mixins.ListModelMixin,
@@ -26,25 +28,33 @@ class QueryViewSet(mixins.CreateModelMixin,
 
     def create(self, request):
         """执行查询接口"""
+        response_context = OrderedDict()
         self.log_debug(
             "request datas: [{request_data}]".format(request_data=request.data))
+        response_context["request_set"] = request.data
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)  # 校验请求参数
         new_query = serializer.save(user=self.request.user)
+        response_context["response_set"] = {"id": new_query.id}
         try:
             query_results = new_query.execute()
-            # new_query.save(
-            #     executed_query=query_results["executed_query"],
-            #     duration=query_results["duration"],
-            #     rows=query_results["rows"],
-            #     status=settings.QUERY_STASTUS[settings.SUCCESS],
-            # )
+            new_query.executed_query = query_results["executed_query"]
+            new_query.duration = query_results["duration"]
+            new_query.rows = query_results["total"]
+            new_query.status = settings.SUCCESS
+            new_query.save()
+            response_context["response_set"].update(query_results)
+            response_context["status"] = settings.SUCCESS
+            response = Response(response_context, status.HTTP_200_OK)
         except Exception as e:
-            raise e
-            # new_query.save(
-            #     status=settings.QUERY_STASTUS[settings.FAILED], error_messages=e)
+            new_query.status = settings.FAILED
+            new_query.error_messages = str(e)
+            new_query.save()
+            response_context["status"] = settings.FAILED
+            response_context["error_message"] = str(e)
+            response = Response(response_context, status.HTTP_400_BAD_REQUEST)
 
-        return Response(query_results)
+        return response
 
     def list(self, request):
         """获取历史查询记录的接口"""
