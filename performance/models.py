30cf5509be882e9fa2bdd27105ed86631671da10
axu4github <axu.home@gmail.com>
2017-06-24 22:59:51 +0800

    添加page_number字段

--- performance/models.py
+++ performance/models.py
@@ -15,6 +15,7 @@ class Query(models.Model):
     """查询表"""
     query = models.TextField(verbose_name="查询内容", blank=False)
     executed_query = models.TextField(verbose_name="执行的查询", default="")
+    page_number = models.IntegerField(verbose_name="页数", default=1)
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
     duration = models.FloatField(verbose_name="执行时长", default=0.0)
     rows = models.IntegerField(verbose_name="查询结果数量", default=0)
@@ -22,11 +23,11 @@ class Query(models.Model):
     error_messages = models.TextField(verbose_name="查询错误", blank=True, default="")
     user = models.ForeignKey(User, verbose_name="执行人")
 
-    def execute(self):
+    def execute(self, page_number=1):
         if self.query is None:
             raise Exception("param query is None.")
 
-        return SparkSQL().sql(self.query)
+        return SparkSQL().sql(self.query, page_number)
 
     def __unicode__(self):
         return self.query
