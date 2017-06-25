4f0aaaee9a0fa576606f6fcbcc461ed804319beb
axu4github <axu.home@gmail.com>
2017-06-25 11:39:19 +0800

    添加RestFrameworkRouter的注释

--- performance/apis/urls.py
+++ performance/apis/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import url, include
 from views import QueryViewSet
 from rest_framework.routers import DefaultRouter
 
-# Create a router and register our viewsets with it.
+# 参考文档：http://www.django-rest-framework.org/api-guide/routers/
+# Example:
+# - URL pattern: ^users/$ Name: 'user-list'
+# - URL pattern: ^users/{pk}/$ Name: 'user-detail'
+# - URL pattern: ^accounts/$ Name: 'account-list'
+# - URL pattern: ^accounts/{pk}/$ Name: 'account-detail'
 router = DefaultRouter()
 router.register(r"querys", QueryViewSet)
 
