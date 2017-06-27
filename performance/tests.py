23558790a5aaf18005177f835632b26033c102d4
anxu <axu.home@gmail.com>
2017-06-27 23:37:40 +0800

    拆分单元测试用例

--- performance/tests.py
+++ performance/tests.py
@@ -12,6 +12,7 @@ from performance.databases.backends.spark.sparksql import SparkSQL
 class BaseSQLParserTest(TestCase):
 
     def test_format_func(self):
+        """测试基础SQL解析器，格式化SQL方法"""
         sql = """
             select username, 
 
@@ -24,6 +25,7 @@ class BaseSQLParserTest(TestCase):
             formated, "SELECT username, passwd, * FROM table_a AS ta, A, B, (SELECT * FROM table_b AS tb)", formated)
 
     def test_formart_main_construction_func(self):
+        """测试基础SQL解析器 获取主结构方法"""
         sql = """
             select username, 
 
@@ -43,6 +45,7 @@ class BaseSQLParserTest(TestCase):
         self.assertTrue(";" not in main_construction, main_construction)
 
     def test_print_main_construction_func(self):
+        """测试基础SQL解析器，打印SQL方法"""
         sql = """
             select username, 
 
@@ -65,6 +68,7 @@ class BaseSQLParserTest(TestCase):
 class SparkSQLParserTest(TestCase):
 
     def test_formart_main_construction_func(self):
+        """测试SparkSQL解析器，获取主结构方法"""
         sql = """
             select username, 
 
@@ -80,28 +84,39 @@ class SparkSQLParserTest(TestCase):
         self.assertTrue(";" not in main_construction, main_construction)
         self.assertTrue("LIMIT" not in main_construction, main_construction)
 
-    def test_generate_execute_sql_func(self):
+    def test_generate_common_execute_sql(self):
+        """测试生成普通查询SQL"""
         sql = "select * from t limit 10;"
         executed_sql = SparkSQLParser(sql).generate_execute_sql()
         self.assertEqual("SELECT * FROM t LIMIT 10", executed_sql)
 
+    def test_generate_common_execute_sql_page_one(self):
+        """测试生成普通查询SQL分页第一页"""
         sql = "select * from t limit 30"
         executed_sql = SparkSQLParser(sql).generate_execute_sql()
         self.assertEqual("SELECT * FROM t LIMIT 10", executed_sql)
 
+    def test_generate_common_execute_sql_page_two(self):
+        """测试生成普通查询SQL分页第二页"""
         sql = "select * from t limit 30;"
         executed_sql = SparkSQLParser(sql, 2).generate_execute_sql()
         self.assertEqual("SELECT * FROM t LIMIT 20", executed_sql)
 
+    def test_generate_common_execute_sql_select(self):
+        """测试生成普通查询SQL含有SELECT条件"""
         sql = "select t.a as a from t"
         executed_sql = SparkSQLParser(sql, 2).generate_execute_sql()
         self.assertEqual("SELECT t.a AS a FROM t LIMIT 20", executed_sql)
 
+    def test_generate_common_execute_sql_where(self):
+        """测试生成普通查询SQL含有WHERE条件"""
         sql = "select * from live_on_hive_csr where csrid = \"asdasdasd\";"
         executed_sql = SparkSQLParser(sql).generate_execute_sql()
         self.assertEqual(
             "SELECT * FROM live_on_hive_csr WHERE csrid = \"asdasdasd\" LIMIT 10", executed_sql)
 
+    def test_generate_execute_sql_special_chars(self):
+        """测试生成查询SQL含有特殊字符"""
         sql = """
             select\n\t*\nfrom\n(select\n\tcsragentid, count(csragentid) as group_csragentid\nfrom\n\tlive_on_hive_csr\ngroup by\n\tcsragentid) t\norder by\n\tt.group_csragentid DESC;
         """
@@ -109,35 +124,46 @@ class SparkSQLParserTest(TestCase):
         self.assertEqual(
             "SELECT * FROM (SELECT csragentid, count(csragentid) AS group_csragentid FROM live_on_hive_csr GROUP BY csragentid) t ORDER BY t.group_csragentid DESC LIMIT 10", executed_sql)
 
-    def test_generate_count_sql_func(self):
+    def test_generate_count_sql_groupby(self):
+        """测试生成查询总数SQL含有GROUP BY（执行count方法）"""
         sql = "select sum(t.a) as sum_a from t group by t.a"
         count_sql = SparkSQLParser(sql).generate_count_sql()
         self.assertEqual(
             "SELECT sum(t.a) AS sum_a FROM t GROUP BY t.a", count_sql[0])
         self.assertTrue(count_sql[1])
 
+    def test_generate_count_sql_orderby(self):
+        """测试生成查询总数SQL含有ORDER BY，带LIMIT（执行count方法）"""
         sql = "select count(t.a) from t group by t.a order by t.b limit 10"
         count_sql = SparkSQLParser(sql).generate_count_sql()
         self.assertEqual(
             "SELECT count(t.a) FROM t GROUP BY t.a LIMIT 10", count_sql[0])
         self.assertTrue(count_sql[1])
 
+    def test_generate_count_sql_limit(self):
+        """测试生成查询总数SQL带LIMIT（执行count方法）"""
         sql = "select * from t limit 10"
         count_sql = SparkSQLParser(sql).generate_count_sql()
         self.assertEqual("SELECT * FROM t LIMIT 10", count_sql[0])
         self.assertTrue(count_sql[1])
 
+    def test_generate_count_sql_orderby_not_count(self):
+        """测试生成查询总数SQL带ORDER BY 不带LIMIT（不执行count方法）"""
         sql = "select * from t order by t.a  "
         count_sql = SparkSQLParser(sql).generate_count_sql()
         self.assertEqual("SELECT COUNT(*) AS cnt FROM t", count_sql[0])
         self.assertTrue(not count_sql[1])
 
+    def test_generate_count_sql_where(self):
+        """测试生成查询总数SQL带WHERE条件"""
         sql = "select * from live_on_hive_csr where csrid = \"asdasdasd\";"
         count_sql = SparkSQLParser(sql).generate_count_sql()
         self.assertEqual(
             "SELECT COUNT(*) AS cnt FROM live_on_hive_csr WHERE csrid = \"asdasdasd\"", count_sql[0])
         self.assertTrue(not count_sql[1])
 
+    def test_generate_count_sql_special_chars(self):
+        """测试生成查询SQL含有特殊字符"""
         sql = """
             select\n\t*\nfrom\n(select\n\tcsragentid, count(csragentid) as group_csragentid\nfrom\n\tlive_on_hive_csr\ngroup by\n\tcsragentid) t\norder by\n\tt.group_csragentid DESC;
         """
