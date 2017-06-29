a9c12f643af6d5724e52e5d4811753c563eb4052
axu4github <axu.home@gmail.com>
2017-06-29 23:30:08 +0800

    添加创建任务页面（iframe）

--- scraping/urls.py
+++ scraping/urls.py
@@ -5,4 +5,6 @@ from scraping import views
 urlpatterns = [
     # /scraping/flat/
     url(r"flat/$", views.flat, name="flat"),
+    # /scraping/create_scraping_task/
+    url(r"create_scraping_task/$", views.create_scraping_task, name="create_scraping_task"),
 ]
--- scraping/views.py
+++ scraping/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def flat(request):
     return render(request, "scraping/flat.html")
+
+def create_scraping_task(request):
+    return render(request, "scraping/create_scraping_task.html")
