dec344572029274980e011e5cf30f3c80a6a838f
axu4github <axu.home@gmail.com>
2017-07-03 21:21:02 +0800

    添加菜单表，并完成第一个测试用例

--- management/models.py
+++ management/models.py
@@ -17,9 +17,27 @@ class User(AbstractUser):
     """
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
     modified = models.DateTimeField(verbose_name="修改时间", auto_now=True)
+    menus = models.ManyToManyField("Menu", verbose_name="用户菜单")
 
     def __unicode__(self):
         return self.username
 
     class Meta:
         ordering = ('-modified',)
+
+
+class Menu(models.Model):
+    """菜单表"""
+
+    name = models.CharField(verbose_name="菜单名称", max_length=20, blank=False)
+    code = models.CharField(verbose_name="菜单代码", max_length=20, blank=False)
+    parent = models.ForeignKey(
+        "self", default=0, blank=False, related_name="child")
+    is_leaf = models.BooleanField(
+        verbose_name="是否为子节点", default=False, blank=False)
+    linkto = models.CharField(verbose_name="链接地址", max_length=50, default="#")
+    created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+    modified = models.DateTimeField(verbose_name="修改时间", auto_now=True)
+
+    def __unicode__(self):
+        return self.name
--- management/tests.py
+++ management/tests.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from management.models import User, Menu
 
-# Create your tests here.
+
+class MenuTablesTest(TestCase):
+    """菜单表的单元测试"""
+
+    def setUp(self):
+        # 首先创建一条记录，为了之后测试修改，查询，删除使用
+        user = User.objects.create_user(
+            "test_menuer", "test_menuer@gmail.com", "test_menuer")
+        menu = Menu.objects.create(name="主节点", code="root")
+        user.menus.add(menu)
+        user.save()
+
+    def test_users_menu(self):
+        user_first_menu = User.objects.filter(
+            username="test_menuer")[0].menus.all()[0]
+        self.assertEqual("主节点", user_first_menu.name)
+        self.assertEqual("root", user_first_menu.code)
