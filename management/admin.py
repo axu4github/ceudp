baeaf6046c64bad53bb9c1eedde7e6a66ae191de
anxu <axu.home@gmail.com>
2017-07-03 23:53:12 +0800

    将菜单表添加到Admin页面

--- management/admin.py
+++ management/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from models import User
+from models import User, Menu
 
 admin.site.register(User)
+admin.site.register(Menu)
