0497f2569dd267d6917ced3f5037ba956930707d
axu4github <axu.home@gmail.com>
2017-07-03 22:05:32 +0800

    添加菜单表自动is_leaf列自动添加方法

--- management/models.py
+++ management/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ObjectDoesNotExist
 
 """
 字段类型参考：
@@ -38,6 +39,16 @@ class Menu(models.Model):
     linkto = models.CharField(verbose_name="链接地址", max_length=50, default="#")
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
     modified = models.DateTimeField(verbose_name="修改时间", auto_now=True)
+
+    def save(self, *args, **kwargs):
+        try:
+            parent = self.parent
+        except ObjectDoesNotExist as e:
+            self.is_leaf = False
+        else:
+            self.is_leaf = True
+
+        super(Menu, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
