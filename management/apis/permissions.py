36b8cffa18ff283cc581da2ebd0d0c55f69dabcd
anxu <axu.home@gmail.com>
2017-07-06 01:43:34 +0800

    完成从自定义权限处获取接口唯一标识

--- management/apis/permissions.py
+++ management/apis/permissions.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from rest_framework import permissions
+from django.core.urlresolvers import resolve
 
 __author__ = "axu"
 
@@ -9,5 +10,12 @@ __author__ = "axu"
 class CustomerAccessPermission(permissions.BasePermission):
     """自定义权限"""
 
+    def api_unique(self, request):
+        """获取接口唯一标识"""
+        return "{http_method}:{view_name}".format(
+            http_method=request.method.lower(),
+            view_name=resolve(request.path).view_name)
+
     def has_permission(self, request, view):
+        # print self.api_unique(request)
         return True
