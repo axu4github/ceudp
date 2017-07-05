0a3b6604672e715721187bd780637759670ea028
anxu <axu.home@gmail.com>
2017-07-05 17:11:44 +0800

    添加用户默认密码配置

--- management/settings.py
+++ management/settings.py
@@ -6,6 +6,7 @@ from appconf import AppConf
 
 __author__ = "axu"
 
+
 class ManagementConfigurtion(AppConf):
     """
     Management应用配置文件
@@ -15,11 +16,15 @@ class ManagementConfigurtion(AppConf):
     - 如果设置和项目配置（django.conf）同样名称的配置项，调用时会使用项目配置文件中指定的配置项，而不会使用当前类中的配置项。
     """
 
+    # 错误提示信息
     ERROR_MESSAGES = {
         "NoneUsernameOrPasswordError": "none username or passoword.",
         "UsernameOrPasswordIncorrectError": "username or password were incorrect.",
         "UserIsDisableError": "user has been disabled.",
     }
+
+    # 用户默认密码
+    USER_DEFAULT_PASSWORD = "123456"
 
     class Meta:
         """如果设置了前缀那么，调用配置的时候也要加上前缀调用。"""
