9e924f2f1552e27741694ae36c9650668d449e3a
anxu <axu.home@gmail.com>
2017-07-14 21:49:51 +0800

    添加Word，Excel，PDF文件内容读取单元测试

--- performance/tests.py
+++ performance/tests.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+import tempfile
 from collections import OrderedDict
 from django.test import TestCase
 from performance.databases.backends.base.sqlparser import BaseSQLParser
 from performance.databases.backends.spark.spark_sqlparser import SparkSQLParser
+# from performance.hdfs_clients import HDFSClient
+from performance.utils import Utils
+from performance.settings import settings
 
 
 class BaseSQLParserTest(TestCase):
@@ -170,6 +175,16 @@ class SparkSQLParserTest(TestCase):
             "SELECT COUNT(*) AS cnt FROM (SELECT csragentid, count(csragentid) AS group_csragentid FROM live_on_hive_csr GROUP BY csragentid) t", count_sql)
         self.assertTrue(not need_count)
 
+    def test_generate_execute_sql_upper_or_lower(self):
+        """测试生成执行查询大小写"""
+        sql = """
+            select * from t where name = 'XiaoQiang';
+        """
+
+        executed_sql = SparkSQLParser(sql).generate_execute_sql()
+        self.assertEqual(
+            "SELECT * FROM t WHERE name = 'XiaoQiang' LIMIT 10", executed_sql)
+
 
 class SparkSQLRunEnvTest(TestCase):
     """Spark SQL 运行环境测试"""
@@ -181,3 +196,169 @@ class SparkSQLClassTest(TestCase):
     def test_sql_func(self):
         # print SparkSQL().sql("select * from people  ;")
         pass
+
+
+# class UnstructuredDataTest(TestCase):
+#     """非结构化数据测试"""
+
+#     def setUp(self):
+#         self.hdfs_client = HDFSClient(settings.HDFS_WEB_URL)
+#         self.uploaded_file = "{0}/{1}".format(
+#             settings.BASE_DIR, "/ceudp/fixtures/unstructured_data.docx")
+
+#     def test_upload_file_to_dir(self):
+#         dest = "/test_upload/"
+#         self.hdfs_client.upload(dest, self.uploaded_file, overwrite=True)
+
+#         dest_full_path = "{0}{1}".format(
+#             dest, os.path.basename(self.uploaded_file))
+#         # 判断文件存在
+#         self.assertTrue(
+#             self.hdfs_client.status(dest_full_path, strict=False) is not None)
+
+#         # 删除已上传文件
+#         self.hdfs_client.delete(dest_full_path)
+
+#         # 验证
+#         self.assertTrue(
+#             self.hdfs_client.status(dest_full_path, strict=False) is None)
+
+#     def test_upload_file(self):
+#         dest_full_path = "/test_upload/renamed_unstructured_data.docx"
+#         self.hdfs_client.upload(
+#             dest_full_path, self.uploaded_file, overwrite=True)
+#         # 判断文件存在
+#         self.assertTrue(
+#             self.hdfs_client.status(dest_full_path, strict=False) is not None)
+
+#         # 删除已上传文件
+#         self.hdfs_client.delete(dest_full_path)
+
+#         # 验证
+#         self.assertTrue(
+#             self.hdfs_client.status(dest_full_path, strict=False) is None)
+
+#     def test_list(self):
+#         dest_full_path = "/test_upload/test_list.docx"
+
+#         # 上传文件并判断文件是否上传成功
+#         self.hdfs_client.upload(
+#             dest_full_path, self.uploaded_file, overwrite=True)
+#         self.assertTrue(
+#             self.hdfs_client.status(dest_full_path, strict=False) is not None)
+
+#         # 查看该文件并对比文件大小是否相同
+#         file_info = self.hdfs_client.list(dest_full_path)
+#         self.assertEqual(
+#             os.path.getsize(self.uploaded_file),
+#             file_info['docs'][0]['length'])
+
+#         # 删除文件并判断文件是否删除成功
+#         self.hdfs_client.delete(dest_full_path)
+#         self.assertTrue(self.hdfs_client.status(
+#             dest_full_path, strict=False) is None)
+
+#     def test_download(self):
+#         import tempfile
+#         (_, local_path) = tempfile.mkstemp()
+#         dest_full_path = "/test_download/test_download.docx"
+#         # 上传文件并判断文件是否上传成功
+#         self.hdfs_client.upload(
+#             dest_full_path, self.uploaded_file, overwrite=True)
+#         self.assertTrue(self.hdfs_client.status(
+#             dest_full_path, strict=False) is not None)
+
+#         self.hdfs_client.download(dest_full_path, local_path)
+#         # 确定文件存在
+#         self.assertTrue(os.path.exists(local_path))
+#         # 确定文件一致
+#         self.assertEqual(os.path.getsize(local_path),
+#                          os.path.getsize(self.uploaded_file))
+
+#         # 删除临时文件
+#         os.remove(local_path)
+
+#         # 删除文件并判断文件是否删除成功
+#         self.hdfs_client.delete(dest_full_path)
+#         self.assertTrue(self.hdfs_client.status(
+#             dest_full_path, strict=False) is None)
+
+#     def test_write(self):
+#         # 读取文件全部内容
+#         filedata = open(self.uploaded_file, 'rb').read()
+#         filename = os.path.basename(self.uploaded_file)
+#         dest_full_path = "/test_write/test_write.docx"
+
+#         # 写内容到hdfs
+#         self.hdfs_client.write(
+#             dest_full_path, data=filedata, filename=filename)
+#         # 查看该文件并对比文件大小是否相同
+#         #!# 注意：现在hdfs中文件名应为 test_write.docx，本地文件名为 unstructured_data.docx
+#         file_info = self.hdfs_client.list(dest_full_path)
+#         self.assertEqual(os.path.getsize(
+#             self.uploaded_file), file_info['docs'][0]['length'])
+
+#         # 删除文件并判断文件是否删除成功
+#         self.hdfs_client.delete(dest_full_path)
+#         self.assertTrue(self.hdfs_client.status(
+#             dest_full_path, strict=False) is None)
+
+#     def test_search(self):
+#         local_path = "{0}/{1}".format(
+#             settings.BASE_DIR, "/ceudp/fixtures/people.txt")
+#         hdfs_path = "/test_search/"
+#         self.hdfs_client.upload(hdfs_path, local_path, overwrite=True)
+#         self.assertTrue(
+#             self.hdfs_client.status(hdfs_path + "people.txt", strict=False) is not None)
+
+#         result = self.hdfs_client.search("Michael", limit=1)
+#         file_name = os.path.basename(local_path)
+#         file_extension = Utils.file_extension(file_name)
+#         file_size = os.path.getsize(local_path)
+
+#         self.assertEqual(result['docs'][0]['filename'], file_name)
+#         self.assertEqual(result['docs'][0]['fileExtension'], file_extension)
+#         self.assertEqual(result['docs'][0]['length'], file_size)
+
+#         self.hdfs_client.delete(hdfs_path + "people.txt")
+#         self.assertTrue(
+#             self.hdfs_client.status(hdfs_path + "people.txt", strict=False) is None)
+
+#         result = self.hdfs_client.search("Michael", limit=1)
+#         self.assertEqual(result['numFound'], 0, result)
+
+
+class UtilsTest(TestCase):
+    '''工具类测试'''
+
+    def test_excel_get_contents(self):
+        excelfile = "{0}/{1}".format(
+            settings.BASE_DIR, "/ceudp/fixtures/callinfo.xlsx")
+        self.assertTrue("dy-gz" in Utils.excel_get_contents(excelfile))
+
+    def test_pdf_get_contents(self):
+        pdffile = "{0}/{1}".format(
+            settings.BASE_DIR, "/ceudp/fixtures/ant_install_doc.pdf")
+        self.assertTrue("ant" in Utils.pdf_get_contents(pdffile))
+
+    def test_docx_get_contents(self):
+        docxfile = "{0}/{1}".format(
+            settings.BASE_DIR, "/ceudp/fixtures/unstructured_data.docx")
+        self.assertTrue("非结构化" in Utils.docx_get_contents(docxfile))
+
+    def test_file_get_contents(self):
+        file = "{0}/{1}".format(
+            settings.BASE_DIR, "/ceudp/fixtures/people.json")
+        self.assertTrue("Michael" in Utils.file_get_contents(file))
+
+    # 自动识别文件类型
+    def test_get_contents(self):
+        file = "{0}/{1}".format(
+            settings.BASE_DIR, "/ceudp/fixtures/ant_install_doc.pdf")
+        self.assertTrue("ant" in Utils.get_contents(file))
+
+    def test_file_put_contexts(self):
+        file = "{0}/{1}".format(tempfile.mkdtemp(), "123.txt")
+        Utils.file_put_contents(file, "123")
+        self.assertTrue(os.path.exists(file))
+        self.assertEqual("123", Utils.get_contents(file))
