c657f0c412338c4abe1d561dd9438a4698c80485
anxu <axu.home@gmail.com>
2017-07-15 13:01:21 +0800

    添加审计日志操作枚举项，以及审计日志unicode方法

--- security/models.py
+++ security/models.py
@@ -9,6 +9,17 @@ from management.models import User
 __author__ = "axu"
 
 
+class ACTION(object):
+    """审计日志操作动作"""
+
+    CREATE = "CREATE"  # 创建
+    UPDATE = "UPDATE"  # 修改
+    DESTROY = "DESTROY"  # 删除
+    API_LOGIN = "API_LOGIN"  # 接口登陆
+    LOGIN = "LOGIN"  # 网页登陆
+    LOGOUT = "LOGOUT"  # 网页登出
+
+
 class AuditLog(models.Model):
     """审计日志表"""
 
@@ -18,6 +29,11 @@ class AuditLog(models.Model):
     action = models.CharField(verbose_name="操作动作", max_length=50, blank=False)
     content = models.TextField(verbose_name="操作内容", default="")
     created = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+
+    def __unicode__(self):
+        return "{user} | {model} | {action} | {content}".format(
+            user=self.user, model=self.content_type.model,
+            action=self.action, content=self.content)
 
     class Meta:
         ordering = ('-created', )
