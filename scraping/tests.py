6b4c52095ce4a19056bba92d348437868eb083d4
anxu <axu.home@gmail.com>
2017-07-17 06:39:36 +0800

    添加任务详情单元测试（未实现）

--- scraping/tests.py
+++ scraping/tests.py
@@ -17,3 +17,20 @@ class ScrapeJobAPITest(TestCase):
 class ScrapeJobAPIPermissionTest(TestCase):
     """采集任务API权限测试（需要补充）"""
     pass
+
+
+class ScrapeJobDetailTableTest(TestCase):
+    """采集任务详情
+
+    表测试（需要补充）"""
+    pass
+
+
+class ScrapeJobDetailAPITest(TestCase):
+    """采集任务详情API测试（需要补充）"""
+    pass
+
+
+class ScrapeJobDetailAPIPermissionTest(TestCase):
+    """采集任务详情API权限测试（需要补充）"""
+    pass
