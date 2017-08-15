13f783b4cc252cb7026a42de1f18ac9bddec1193
axu4github <axu.home@gmail.com>
2017-08-15 21:12:48 +0800

    单引号修改为双引号

--- performance/apis/urls.py
+++ performance/apis/urls.py
@@ -6,20 +6,20 @@ from rest_framework.routers import DefaultRouter
 
 # 参考文档：http://www.django-rest-framework.org/api-guide/routers/
 # Example:
-# - URL pattern: ^users/$ Name: 'user-list'
-# - URL pattern: ^users/{pk}/$ Name: 'user-detail'
-# - URL pattern: ^accounts/$ Name: 'account-list'
-# - URL pattern: ^accounts/{pk}/$ Name: 'account-detail'
+# - URL pattern: ^users/$ Name: "user-list"
+# - URL pattern: ^users/{pk}/$ Name: "user-detail"
+# - URL pattern: ^accounts/$ Name: "account-list"
+# - URL pattern: ^accounts/{pk}/$ Name: "account-detail"
 router = DefaultRouter()
 router.register(r"querys", QueryViewSet)
 
 urlpatterns = [
     url(r"^", include(router.urls)),
-    url(r'unstructured_datas/$',
+    url(r"unstructured_datas/$",
         UnstructuredDataViewSet.as_view(
-            {'post': 'create', 'get': 'list', 'delete': 'destroy'}),
-        name='unstructured_datas'),
-    url(r'unstructured_datas/search/$',
-        UnstructuredDataViewSet.as_view({'get': 'search'}),
-        name='unstructured_search'),
+            {"post": "create", "get": "list", "delete": "destroy"}),
+        name="unstructured_datas"),
+    url(r"unstructured_datas/search/$",
+        UnstructuredDataViewSet.as_view({"get": "search"}),
+        name="unstructured_search"),
 ]
